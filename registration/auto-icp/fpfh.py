450dce00da52bc27bc0fa1feed19aaae2d1be681
chenyifanthu <863506811@qq.com>
2021-09-06 21:53:48 +0800

    0906

--- registration/auto-icp/fpfh.py
+++ registration/auto-icp/fpfh.py
@@ -2,6 +2,7 @@ import open3d as o3d
 from sklearn.neighbors import NearestNeighbors, DistanceMetric, BallTree
 import numpy as np
 import time
+import random
 from tqdm import tqdm
     
 
@@ -34,7 +35,7 @@ def find_correspondence(xyz1, xyz2, feat1, feat2, h_threshold=0.05):
     sort_idx = np.argsort(xyz2[:, 2])
     xyz2 = xyz2[sort_idx, :]
     feat2 = feat2[sort_idx, :]
-    corr_list = []
+    corr_pairs1, corr_pairs2 = [], []
     for i in tqdm(range(xyz1.shape[0])):
         left, right = valid_height_in_range(xyz2[:, 2], 
                                             xyz1[i, 2] - h_threshold, 
@@ -42,9 +43,10 @@ def find_correspondence(xyz1, xyz2, feat1, feat2, h_threshold=0.05):
         
         if left != -1:
             distances = np.linalg.norm(feat1[i, :] - feat2[left:right, :], axis=1)
-            corr_list.append((i, sort_idx[left+np.argmin(distances)]))
+            corr_pairs1.append(i)
+            corr_pairs2.append(sort_idx[left+np.argmin(distances)])
             
-    return corr_list
+    return np.array(corr_pairs1), np.array(corr_pairs2)
 
 
 def fpfh(pcd):
@@ -57,12 +59,37 @@ def fpfh(pcd):
     
     return fpfh.data.T
 
-if __name__ == '__main__':
+def solve_pnp_ransac(source, target, 
+                     corr_pairs1, corr_pairs2, 
+                     ransac_n=3, step=1000, 
+                     distance_threshold=0.5):
+    assert len(corr_pairs1) == len(corr_pairs2)
+    best_R, best_t, best_inlier = None, None, -1
+    for _ in tqdm(range(step)):
+        choose_id = random.sample(range(len(corr_pairs1)), ransac_n)
+        source_ = source[corr_pairs1[choose_id], :].T
+        target_ = target[corr_pairs2[choose_id], :].T
+        center1 = np.mean(source_, axis=1, keepdims=True)
+        center2 = np.mean(target_, axis=1, keepdims=True)
+        W = (source_ - center1).dot((target_ - center2).T)
+        u, _, vt = np.linalg.svd(W)
+        R = u.dot(vt)
+        t = center2 - R.dot(center1)
+        # target_hat = R.dot(source.T) + t
+        # distance = np.linalg.norm(target - target_hat.T, axis=1)
+        # inlier_n = sum(distance < distance_threshold)
+        # if inlier_n > best_inlier:
+        #     best_R, best_t, best_inlier = R, t, inlier_n
+        
+    # return best_R, best_t
+    
 
+if __name__ == '__main__':
     pcd = o3d.io.read_point_cloud('data/20210808/leica_down.ply')
     # o3d.visualization.draw_geometries([pcd])
     xyz = np.asarray(pcd.points)
     feat = fpfh(pcd)
-    corr_list = find_correspondence(xyz, xyz.copy(), feat, feat.copy())
-    print(corr_list)
+    corr_pairs1, corr_pairs2 = find_correspondence(xyz, xyz.copy(), feat, feat.copy())
+    solve_pnp_ransac(xyz[:, :2], xyz[:, :2], corr_pairs1, corr_pairs2)
+    # print(corr_pairs2)
 
